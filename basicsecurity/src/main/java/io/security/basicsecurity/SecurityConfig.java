6c933bc1115bd47a246845abf3afcfaed120e1ca
Gwak seyeon <ksyk1205@naver.com>
2022-01-26 15:27:10 +0900

    Form login 인증 API & 필터

--- basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -4,6 +4,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @Configuration
 @EnableWebSecurity
@@ -11,9 +20,32 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //인가
-        http.authorizeHttpRequests() //요청에 대한 보안 검사 시작
+        http.authorizeRequests() //요청에 대한 보안 검사 시작
                 .anyRequest().authenticated(); //어떤 요청에도 인증을 받도록 설정
         //인증
-        http.formLogin();
+        http.formLogin()
+                //.loginPage("/loginPage")
+                .defaultSuccessUrl("/")
+                .failureUrl("/loginPage")
+                .usernameParameter("userId")
+                .passwordParameter("passwd")
+                .loginProcessingUrl("/login_proc")
+//                .successHandler(new AuthenticationSuccessHandler() {
+//                    @Override
+//                    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication)
+//                            throws IOException, ServletException {
+//                        System.out.println("authentication "+ authentication.getName());
+//                        response.sendRedirect("/");
+//                    }
+//                })
+//                .failureHandler(new AuthenticationFailureHandler() {
+//                    @Override
+//                    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
+//                            throws IOException, ServletException {
+//                        System.out.println("exception "+exception.getMessage());
+//                        response.sendRedirect("/login");
+//                    }
+//                })
+                .permitAll(); //인증된 사용자가 아니더라도 loginPage에 접근이 가능하도록
     }
 }
