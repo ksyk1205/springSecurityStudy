5f9a04cacdcb498a03bfeeb94064df55cb956539
Gwak seyeon <ksyk1205@naver.com>
2022-02-07 22:51:34 +0900

    인가처리

--- basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -5,32 +5,21 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 
 @Configuration
 @EnableWebSecurity
-@Order(0)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     protected  void configure(HttpSecurity http) throws Exception{
         http
-                .antMatcher("/admin/**")
                 .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .httpBasic();
-    }
-}
-
-@Configuration
-@Order(1)
-class SecurityConfig2 extends  WebSecurityConfigurerAdapter{
-    protected  void configure(HttpSecurity http) throws Exception{
+                .anyRequest().authenticated();
         http
-                .authorizeRequests()
-                .anyRequest().permitAll()
-                .and()
                 .formLogin();
-    }
 
+        SecurityContextHolder.setStrategyName(SecurityContextHolder.MODE_INHERITABLETHREADLOCAL);
+    }
 }
+
--- basicsecurity/src/main/java/io/security/basicsecurity/SecurityController.java
+++ basicsecurity/src/main/java/io/security/basicsecurity/SecurityController.java
@@ -1,38 +1,39 @@
 package io.security.basicsecurity;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
 
 @RestController
 public class SecurityController {
 
     @GetMapping("/")
-    public String index(){
+    public String index(HttpSession session){
+        //authentication과 authentication1 은 동일한 객체를 참조
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        SecurityContext context = (SecurityContext)session.getAttribute(HttpSessionSecurityContextRepository.SPRING_SECURITY_CONTEXT_KEY);
+        Authentication authentication1 = context.getAuthentication();
         return "home";
     }
 
-    @GetMapping("/user")
-    public String user(){
-     return "user";
+    @GetMapping("/thread")
+    public String thread(){
+
+        new Thread(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                    }
+                }
+        ).start();
+        return "thread";
+
     }
 
-    @GetMapping("/admin/pay")
-    public String adminPay(){
-        return "adminPay";
-    }
-
-    @GetMapping("/admin/**")
-    public String admin(){
-        return "admin";
-    }
-
-    @GetMapping("/denied")
-    public String denied(){
-        return "Access is denied";
-    }
-
-    @GetMapping("/login")
-    public String login(){
-        return "login";
-    }
 }
